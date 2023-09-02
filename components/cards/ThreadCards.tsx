fd5b261ec660ccc81532c2f7b171a1012112bda8
Elsa <elsahvy@gmail.com>
2023-09-02 10:42:39 -0700

    grid adjust

--- components/cards/ThreadCards.tsx
+++ components/cards/ThreadCards.tsx
@@ -3,12 +3,12 @@ import content from "../data";
 
 const ThreadCards = () => {
   return (
-    <section className=" grid 2xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6 p-5">
+    <section className="grid 2xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6 mr-5 px-[25px] py-[50px]">
       {content.map((item, index) => (
         <div className="bg-teal rounded-lg" key={index}>
           <div className="underline pl-5 pt-2 text-seasalt">tags</div>
 
-          <Image className="py-1 px-5 h-[300px]" src={item.image} alt="" />
+          <Image className="py-1 px-5 h-[350px]" src={item.image} alt="" />
           <div className="px-5 pb-5">
             <h2 className="text-lavender font-bold pb-2">{item.title}</h2>
             <div className="py-2">{item.paragraph}</div>
